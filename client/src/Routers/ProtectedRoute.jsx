8d3f79341d2afba7318838e7627b89580ae09d18
abdul basith <BasithDev>
2024-11-02 20:53:42 +0530

    added propstype for authRoute.jsx and protectedRoute.jsx

--- client/src/Routers/ProtectedRoute.jsx
+++ client/src/Routers/ProtectedRoute.jsx
@@ -1,11 +1,18 @@
+import PropTypes from 'prop-types';
 import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectedRoute = ({ isAuthenticated , forAdmin }) => {
+const ProtectedRoute = ({ isAuthenticated, forAdmin }) => {
     if (forAdmin) {
-        return isAuthenticated ? <Outlet /> : <Navigate to="/admin/login" />;   
-    }else{
-        return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;   
+        return isAuthenticated ? <Outlet /> : <Navigate to="/admin/login" />;
+    } else {
+        return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
     }
+};
+
+// Define prop types
+ProtectedRoute.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    forAdmin: PropTypes.bool.isRequired,
 };
 
 export default ProtectedRoute;
