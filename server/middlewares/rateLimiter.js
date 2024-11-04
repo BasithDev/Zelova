71bdd1811b9ee68fe71e320af2fb780d44471c53
BasithDev <sykickbasith@gmail.com>
2024-11-04 20:29:41 +0530

    added a popup to make user and vendor login seperate make the vendor to login

--- server/middlewares/rateLimiter.js
+++ server/middlewares/rateLimiter.js
@@ -1,6 +1,6 @@
 const rateLimit = require('express-rate-limit');
 const loginLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
+    windowMs: 1 * 6 * 1000,
     max: 5,
     message: "Too many login attempts, please try again later."
 });
