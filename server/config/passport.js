21950284fe46ce007abdcab18fde02b7ca521795
BasithDev <sykickbasith@gmail.com>
2024-11-04 09:55:43 +0530

    added redux to the app and implemented admin login - 3/11/24

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,7 +6,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SEC,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: '/api/user/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
