42a0f7720185eee3c77d613deda73d9f76ea52db
abdul basith <BasithDev>
2024-11-19 21:48:58 +0530

    added image selection in client and preview and re-dit and also crop feature

--- server/utils/jwtDecoder.js
+++ server/utils/jwtDecoder.js
@@ -1,12 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 const decodeToken = (token, secret) => {
-    try {
-        return jwt.verify(token, secret);
-    } catch (error) {
-        console.error("Token decoding error:", error.message);
-        return null;
+    if (token) {
+        try {
+            return jwt.verify(token, secret);
+        } catch (error) {
+            console.error("Token decoding error:", error.message);
+            return null;
+        } 
+    }else{
+        console.log('token expired or is not available')
+        return null
     }
+    
 };
 
 module.exports = decodeToken;
