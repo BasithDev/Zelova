7095210cc097b5ff2de887666584efdfe3244f84
BasithDev <sykickbasith@gmail.com>
2024-12-11 20:03:57 +0530

    added resturant rating feature through previous orders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import UserLayout from './Components/Layouts/UserLayout';
 import AdminLayout from './Components/Layouts/AdminLayout';
@@ -57,81 +59,84 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <AuthChecker />
-          <AdminAuthChecker/>
-            <Routes>
-              {/* Public User Routes */}
-              <Route element={<UserNoAuthRoute />}>
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
-                <Route path="otp" element={<Otp />} />
-                <Route path="google-response" element={<GoogleResponse />} />
-                <Route path='forgot-password' element={<ForgotPassword/>}/>
-              </Route>
+    <div className="App">
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <AuthChecker />
+            <AdminAuthChecker/>
+              <Routes>
+                {/* Public User Routes */}
+                <Route element={<UserNoAuthRoute />}>
+                  <Route path="login" element={<Login />} />
+                  <Route path="register" element={<Register />} />
+                  <Route path="otp" element={<Otp />} />
+                  <Route path="google-response" element={<GoogleResponse />} />
+                  <Route path='forgot-password' element={<ForgotPassword/>}/>
+                </Route>
 
-              {/* Public Admin Routes */}
-              <Route element={<AdminNoAuthRoute />}>
-                <Route path="/admin/login" element={<AdminLogin />} />
-              </Route>
+                {/* Public Admin Routes */}
+                <Route element={<AdminNoAuthRoute />}>
+                  <Route path="/admin/login" element={<AdminLogin />} />
+                </Route>
 
-              {/* User Routes */}
-              <Route element={<UserProtectedRoute />}>
-              <Route path="/role-select" element={<RoleManagement />} />
-                <Route path="/" element={<UserLayout />}>
-                  <Route index element={<Home />} />
-                  <Route path="profile" element={<Profile />} />
-                  <Route path="edit-user" element={<EditUser/>} />
-                  <Route path="change-id" element={<EditId/>} />
-                  <Route path="reset-password" element={<ResetPassword/>}/>
-                  <Route path="request-vendor" element={<RequestVendorPage />} />
-                  <Route path='favourites' element={<Favourites/>} />
-                  <Route path='orders' element={<UserOrderPage/>} />
-                  <Route path='coins' element={<Coins/>} />
-                  <Route path='share-supplies' element={<ShareSupplies/>} />
-                  <Route path='get-supplies' element={<GetSupplies/>}/>
-                  <Route path='address-manage' element={<AddressMng/>}/>
-                  <Route path='/restaurant/:id/menu' element={<Menu/>}/>
-                  <Route path='cart' element={<Cart/>} />
-                  <Route path='confirm-payment' element={<ConfirmPayment/>}/>
-                  <Route path='report' element={<Report/> }/>
-                  <Route element={<OrderProtectedRoute />}>
-                    <Route path='order-success' element={<OrderSuccess/>}/>
+                {/* User Routes */}
+                <Route element={<UserProtectedRoute />}>
+                <Route path="/role-select" element={<RoleManagement />} />
+                  <Route path="/" element={<UserLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="edit-user" element={<EditUser/>} />
+                    <Route path="change-id" element={<EditId/>} />
+                    <Route path="reset-password" element={<ResetPassword/>}/>
+                    <Route path="request-vendor" element={<RequestVendorPage />} />
+                    <Route path='favourites' element={<Favourites/>} />
+                    <Route path='orders' element={<UserOrderPage/>} />
+                    <Route path='coins' element={<Coins/>} />
+                    <Route path='share-supplies' element={<ShareSupplies/>} />
+                    <Route path='get-supplies' element={<GetSupplies/>}/>
+                    <Route path='address-manage' element={<AddressMng/>}/>
+                    <Route path='/restaurant/:id/menu' element={<Menu/>}/>
+                    <Route path='cart' element={<Cart/>} />
+                    <Route path='confirm-payment' element={<ConfirmPayment/>}/>
+                    <Route path='report' element={<Report/> }/>
+                    <Route element={<OrderProtectedRoute />}>
+                      <Route path='order-success' element={<OrderSuccess/>}/>
+                    </Route>
                   </Route>
                 </Route>
-              </Route>
 
-              {/* Vendor Routes */}
-              <Route element={<UserRoleProtectedRoute allowedRoles={['vendor']} />}>
-                <Route path="/vendor" element={<VendorLayout />}>
-                <Route index element={<VendorHome/>} />
-                  <Route path="add-items" element={<AddItem />} />
-                  <Route path='manage-restaurant' element={<ManageRestaurant/>}/>
-                  <Route path='menu' element={<MenuManagement/>}/> 
-                  <Route path='orders' element={<Orders/>}/> 
+                {/* Vendor Routes */}
+                <Route element={<UserRoleProtectedRoute allowedRoles={['vendor']} />}>
+                  <Route path="/vendor" element={<VendorLayout />}>
+                  <Route index element={<VendorHome/>} />
+                    <Route path="add-items" element={<AddItem />} />
+                    <Route path='manage-restaurant' element={<ManageRestaurant/>}/>
+                    <Route path='menu' element={<MenuManagement/>}/> 
+                    <Route path='orders' element={<Orders/>}/> 
+                  </Route>
                 </Route>
-              </Route>
 
-              {/* Admin Routes */}
-              <Route element={<AdminRoleProtectedRoute />}>
-                <Route path="/admin" element={<AdminLayout />}>
-                  <Route index element={<Dashboard />} />
-                  <Route path="user-manage" element={<UserManagement />} />
-                  <Route path="vendor-manage" element={<SellerManagement />} />
-                  <Route path="requests" element={<Requests />} />
-                  <Route path='category-manage' element={<CategoryMng/>}/>
-                  <Route path='coupon-manage' element={<CouponMng/>}/> 
-                  <Route path="send-mail" element={<SendMail/>} />
+                {/* Admin Routes */}
+                <Route element={<AdminRoleProtectedRoute />}>
+                  <Route path="/admin" element={<AdminLayout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="user-manage" element={<UserManagement />} />
+                    <Route path="vendor-manage" element={<SellerManagement />} />
+                    <Route path="requests" element={<Requests />} />
+                    <Route path='category-manage' element={<CategoryMng/>}/>
+                    <Route path='coupon-manage' element={<CouponMng/>}/> 
+                    <Route path="send-mail" element={<SendMail/>} />
+                  </Route>
                 </Route>
-              </Route>
-              
-            </Routes>
-          
-        </Router>
-      </QueryClientProvider>
-    </GoogleOAuthProvider>
+                
+              </Routes>
+            
+          </Router>
+        </QueryClientProvider>
+      </GoogleOAuthProvider>
+      <ToastContainer />
+    </div>
   );
 }
 
--- server/routes/user/ordersMngRoute.js
+++ server/routes/user/ordersMngRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { placeOrder, getCurrentOrders, updateOrderStatus, getPreviousOrdersOnDate ,createRazorpayOrder , verifyRazorpayPayment } = require('../../controllers/user/ordersMng');
+const { placeOrder, getCurrentOrders, updateOrderStatus, getPreviousOrdersOnDate ,createRazorpayOrder , verifyRazorpayPayment,rateRestaurant } = require('../../controllers/user/ordersMng');
 
 router.post('/place-order', placeOrder);
 router.get('/current',getCurrentOrders)
@@ -8,5 +8,6 @@ router.patch('/update-status',updateOrderStatus)
 router.get('/previous/:date',getPreviousOrdersOnDate)
 router.post('/create-razorpay-order', createRazorpayOrder);
 router.post('/verify-razorpay-payment', verifyRazorpayPayment);
+router.post('/rate-restaurant', rateRestaurant)
 
 module.exports = router;
